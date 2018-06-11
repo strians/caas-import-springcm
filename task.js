9b7c3a46fff42dd12e539cf6a94b1d0e1a1b7753
Paul Holden <pholden@stria.com>
2018-06-11 08:55:57 -0700

    Add recursing

--- task.js
+++ task.js
@@ -38,41 +38,124 @@ module.exports = (task, callback) => {
 
       async.eachSeries(_.get(task, 'paths'), (path, callback) => {
         var remote = _.get(path, 'remote');
+        var recurse = _.get(path, 'recurse');
 
-        springCm.getDocuments(remote, (err, documents) => {
-          // If there's an error scraping this directory, skip and continue
-          if (err) {
-            winston.error(err);
+        var documentQueue = async.queue((doc, callback) => {
+          async.waterfall([
+            (callback) => {
+              /**
+               * Send request for the doc (listing doesn't include path)
+               */
 
-            return callback();
-          }
+              springCm.getDocument(doc, callback);
+            },
+            (doc, callback) => {
+              springCm.downloadDocument(doc, fs.createWriteStream(doc.getUid() + '.pdf'), (err) => {
+                if (err) {
+                  winston.error(err);
+                  return callback();
+                }
 
-          // Filter listed documents
-          var included = filter(_.get(path, 'filter'), documents);
+                winston.info('Downloaded', doc.getPath(), '-->', doc.getUid() + '.pdf');
 
-          winston.info('Found', included.length, 'document(s) to download');
+                var wastebin = _.get(path, 'wastebin');
 
-          // Download each document and move to wastebin (delete by default)
-          async.eachSeries(included, (doc, callback) => {
-            springCm.downloadDocument(doc, fs.createWriteStream(doc.getUid() + '.pdf'), (err) => {
-              if (err) {
-                winston.error(err);
-                return callback();
-              }
-
-              winston.info('Downloaded', doc.getName(), '-->', doc.getUid() + '.pdf');
-
-              var wastebin = _.get(path, 'wastebin');
-
-              // Import directory can't be wastebin
-              if (wastebin && wastebin !== remote) {
-                springCm.moveDocument(doc, wastebin, callback);
-              } else {
-                springCm.deleteDocument(doc, callback);
-              }
-            });
-          }, callback);
+                // Import directory can't be wastebin
+                if (wastebin && wastebin !== remote) {
+                  springCm.moveDocument(doc, wastebin, callback);
+                } else {
+                  springCm.deleteDocument(doc, callback);
+                }
+              });
+            }
+          ], callback);
         });
+
+        var folderQueue = async.queue((f, callback) => {
+          var folder;
+
+          async.waterfall([
+            (callback) => {
+              /**
+               * Retrieve folder object (subfolders don't include paths)
+               */
+
+              springCm.getFolder(f, (err, res) => {
+                if (err) {
+                  return callback(err);
+                }
+
+                // Map to full folder object
+                folder = res;
+
+                callback();
+              });
+            },
+            (callback) => {
+              /**
+               * Get a listing of documents in the folder and push them
+               * into the document queue.
+               */
+
+              springCm.getDocuments(folder, (err, documents) => {
+                if (err) {
+                  return callback(err);
+                }
+
+                // Filter listed documents
+                var included = filter(_.get(path, 'filter'), documents);
+
+                winston.info('Found', included.length, 'document(s) in', folder.getPath(), 'to download');
+
+                // Push each document in the document download queue
+                _.each(included, doc => documentQueue.push(doc));
+
+                callback();
+              });
+            },
+            (callback) => {
+              /**
+               * If we're recursing through the remote directory, get
+               * any subfolders and push them into the folder queue
+               */
+
+              if (recurse) {
+                springCm.getSubfolders(folder, (err, folders) => {
+                  if (err) {
+                    return callback(err);
+                  }
+
+                  _.each(folders, folder => folderQueue.push(folder));
+
+                  callback();
+                });
+              } else {
+                callback();
+              }
+            }
+          ], callback);
+        });
+
+        // Add to queue
+        folderQueue.push(remote);
+
+        // Wait for both queues to complete
+        async.waterfall([
+          (callback) => {
+            if (folderQueue.length() > 0) {
+              folderQueue.drain = callback;
+            } else {
+              callback();
+            }
+          },
+          (callback) => {
+            if (documentQueue.length() > 0) {
+              documentQueue.drain = callback;
+            } else {
+              callback();
+            }
+          }
+        ], callback);
       }, callback);
     }
   ], (err) => {
